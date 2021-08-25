a1f71881cae6b8c348f66a3d1f60ba65618338e2
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2021-08-25 12:20:26 -0300

    Adding catch on create to prevent conflict issues

--- src/persist/db/index.js
+++ src/persist/db/index.js
@@ -1,5 +1,5 @@
 import PouchDB from 'pouchdb'
-import { catchError } from "../exceptions";
+import { catchError, ConflictException } from "../exceptions";
 
 function localDatabase () {
   const db = new PouchDB('state')
