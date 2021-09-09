18274ce4d1eb94ae640e1c765e8821e56f5ec241
Gabriel Santiago Cardoso Rizzo <gabriel96.gsr@gmail.com>
2021-09-09 12:39:23 -0300

    Adding bundle split on perse-store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { store } from './yoox'
-import { VueInstall } from './plugins/vue.js'
 
-export default { store, VueInstall }
+export default { store }
 
-export { store, VueInstall }
+export { store }
